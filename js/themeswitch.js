1d38bc0163c72c7c4856cd4efe93b0ad0a81e581
Takase <decaen.pro@gmail.com>
2020-05-27 14:53:26 +0200

    update

--- js/themeswitch.js
+++ js/themeswitch.js
@@ -4,23 +4,32 @@
 function setTheme(themeName) {
     localStorage.setItem('theme', themeName);
     document.documentElement.className = themeName;
-	if (themeName === 'theme-light') {
+	if (themeName === 'theme-light') { 
 		document.getElementById("theme_picture").setAttribute("src", "img/all/moon.png");
-		document.getElementById("yt").setAttribute("src", "img/index/yt_light.png");
-		document.getElementById("twitter").setAttribute("src", "img/index/twitter_light.png");
-		document.getElementById("twitter1").setAttribute("src", "img/index/twitter_light.png");
-		document.getElementById("git").setAttribute("src", "img/index/git_light.png");
-		document.getElementById("discord").setAttribute("src", "img/index/discord_light.png");
-		document.getElementById("arrow").setAttribute("src", "img/index/arrow_light.png");
-	}
-	else {
-		document.getElementById("theme_picture").setAttribute("src", "img/all/sun.png");
-		document.getElementById("yt").setAttribute("src", "img/index/yt.png");
-		document.getElementById("twitter").setAttribute("src", "img/index/twitter.png");
-		document.getElementById("twitter1").setAttribute("src", "img/index/twitter.png");
-		document.getElementById("git").setAttribute("src", "img/index/git.png");
-		document.getElementById("discord").setAttribute("src", "img/index/discord.png");
-		document.getElementById("arrow").setAttribute("src", "img/index/arrow.png");
+		try {
+			document.getElementById("yt").setAttribute("src", "img/index/yt_light.png");
+			document.getElementById("twitter").setAttribute("src", "img/index/twitter_light.png");
+			document.getElementById("twitter1").setAttribute("src", "img/index/twitter_light.png");
+			document.getElementById("git").setAttribute("src", "img/index/git_light.png");
+			document.getElementById("discord").setAttribute("src", "img/index/discord_light.png");
+			document.getElementById("arrow").setAttribute("src", "img/index/arrow_light.png");
+			document.getElementById("arrow1").setAttribute("src", "img/index/arrow_light.png");
+			document.getElementById("arrow2").setAttribute("src", "img/index/arrow_light.png");
+			document.getElementById("arrow3").setAttribute("src", "img/index/arrow_light.png");
+		}
+	} else {
+		document.getElementsByClassName("theme_picture").setAttribute("src", "img/all/sun.png");
+		try {
+			document.getElementById("yt").setAttribute("src", "img/index/yt.png");
+			document.getElementById("twitter").setAttribute("src", "img/index/twitter.png");
+			document.getElementById("twitter1").setAttribute("src", "img/index/twitter.png");
+			document.getElementById("git").setAttribute("src", "img/index/git.png");
+			document.getElementById("discord").setAttribute("src", "img/index/discord.png");
+			document.getElementById("arrow").setAttribute("src", "img/index/arrow.png");
+			document.getElementById("arrow1").setAttribute("src", "img/index/arrow.png");
+			document.getElementById("arrow2").setAttribute("src", "img/index/arrow.png");
+			document.getElementById("arrow3").setAttribute("src", "img/index/arrow.png");
+		}
 	}
 }
 // function to toggle between light and dark theme
