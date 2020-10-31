8c9a03cb561a443abd559d4a3c2b97b506e38838
arthurtakase <arthur.decaen@epitech.eu>
2020-10-31 16:51:52 +0100

    Update deviantart.js

--- js/deviantart.js
+++ js/deviantart.js
@@ -1,3 +1,12 @@
+if (!window.location.hash.contains("access_token=")) {
+    window.location = `https://www.deviantart.com/oauth2/authorize?response_type=token&client_id=13754&redirect_uri=${window.location.origin + window.location.pathname}&state=coucou`;
+}
+
+function get_token() {
+    const match = window.location.hash.match("access_token=([^&]+)");
+    return (match ? match[1] : match)
+}
+
 async function fetch_data(url) {
     const response = fetch(url, {mode: 'no-cors'});
     const data = await response.json();
@@ -5,7 +14,7 @@ async function fetch_data(url) {
 }
 
 async function get_images() {
-    const token = fetch_data("https://www.deviantart.com/oauth2/token?client_id=13754&client_secret=61112f58a45d9c5ae98ff3bfbc29571d&grant_type=client_credentials").access_token;
+    const token = get_token();
     const gallery = "BCFE40F1-C2FF-9DDA-30E5-B18D1C3695B4";
     const url = `https://www.deviantart.com/api/v1/oauth2/gallery/${gallery}?username=Takaaase&access_token=${token}&limit=24`;
     let picture = fetch_data(url);
