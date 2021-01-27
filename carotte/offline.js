5044b2f74602268b15cea3d3c52d2237cc37afcb
Takase <decaen.pro@gmail.com>
2021-01-27 23:06:28 +0100

    add carotte

---
+++ carotte/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1611784974,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/joueur-sheet0.png",
+		"images/carottes-sheet0.png",
+		"images/background-sheet0.png",
+		"images/menu-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
