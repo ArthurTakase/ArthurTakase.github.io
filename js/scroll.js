d26c78e24abc01ebaf89d8f77c03cd0c2426bf46
ArthurTakase <arthur.decaen@epitech.eu>
2022-03-28 22:38:54 +0200

    FEAT: infinite carousel

--- js/scroll.js
+++ js/scroll.js
@@ -1,11 +1,21 @@
 // function that takes dive id as parameter and horizontaly scrolls it's own len to the right
 function scrollRightByVisible(id) {
     var dive = document.getElementById(id);
+    var max = dive.scrollWidth - dive.clientWidth;
+
     dive.scrollLeft += dive.clientWidth;
+
+    if (dive.scrollLeft >= max)
+        dive.scrollLeft = 0;
 }
 
 // function that takes dive id as parameter and horizontaly scrolls it's own len to the left
 function scrollLeftByVisible(id) {
     var dive = document.getElementById(id);
+    var max = dive.scrollWidth - dive.clientWidth;
+
     dive.scrollLeft -= dive.clientWidth;
+
+    if (dive.scrollLeft <= 0)
+        dive.scrollLeft = max;
 }
