3b149214063a3c6bb74f51e7d380b9ef987f95ec
arthurtakase <arthur.decaen@epitech.eu>
2022-09-03 21:21:19 +0200

    FEAT: generate projects cards from json

---
+++ js/projects.js
@@ -0,0 +1,241 @@
+var projets = [{
+        "type": "pro",
+        "title": "Carttet",
+        "description": "Job application allowing agents to transmit their unused vacation days to human resources.",
+        "tech": [{
+                "name": "HTML5",
+                "logo": "<i class='fab fa-html5'></i>",
+            },
+            {
+                "name": "CSS3",
+                "logo": "<i class='fab fa-css3-alt'></i>",
+            },
+            {
+                "name": "Groovy",
+                "logo": "<i class='fa-brands fa-java'></i></i>",
+            }
+        ],
+        "link": [],
+        "gif": "img/gif/Carttet.gif",
+        "img": "https://zupimages.net/up/21/32/vzvv.png"
+    },
+    {
+        "type": "pro",
+        "title": "Ægir & Rán",
+        "description": "Plugin and WebService to track and graph the usage practices of the Departmental Council applications. The goal is to see if the added options are used enough.",
+        "tech": [{
+                "name": "HTML5",
+                "logo": "<i class='fab fa-html5'></i>",
+            },
+            {
+                "name": "CSS3",
+                "logo": "<i class='fab fa-css3-alt'></i>",
+            },
+            {
+                "name": "Groovy",
+                "logo": "<i class='fa-brands fa-java'></i></i>",
+            }, {
+                "name": "JS",
+                "logo": "<i class='fab fa-js-square'></i>"
+            }
+        ],
+        "link": [],
+        "gif": "img/gif/Aegir.gif",
+        "img": "https://zupimages.net/up/21/39/xdhp.jpg"
+    },
+    {
+        "type": "pro",
+        "title": "DSIN Plan",
+        "description": "Dynamic map to obtain in real time the position of the agents in the offices of the department.",
+        "tech": [{
+                "name": "HTML5",
+                "logo": "<i class='fab fa-html5'></i>",
+            },
+            {
+                "name": "CSS3",
+                "logo": "<i class='fab fa-css3-alt'></i>",
+            },
+            {
+                "name": "Groovy",
+                "logo": "<i class='fa-brands fa-java'></i></i>",
+            }, {
+                "name": "JS",
+                "logo": "<i class='fab fa-js-square'></i>"
+            }
+        ],
+        "link": [],
+        "gif": "img/gif/dsinplan.gif",
+        "img": "https://zupimages.net/up/21/42/7wxm.png"
+    },
+    {
+        "type": "perso",
+        "title": "Blue.",
+        "description": "VR Cardboard game about the color blue.",
+        "tech": [{
+            "name": "Unity",
+            "logo": "<i class='fa-brands fa-unity'></i>",
+        }],
+        "link": [{
+            "url": "https://mega.nz/file/wo8mURDK#ZGF02X2i0cFEWYwWF9nCGJvDBwOzJBlzLk3PYVLNZhU",
+            "logo": "<i class='fa-solid fa-download'></i>"
+        }],
+        "gif": "img/gif/blue.gif",
+        "img": "img/projects/blue.png "
+    },
+    {
+        "type": "perso",
+        "title": "Heros.js",
+        "description": "JS game engine for Visual Novel games.",
+        "tech": [{
+                "name": "HTML5",
+                "logo": "<i class='fab fa-html5'></i>",
+            },
+            {
+                "name": "CSS3",
+                "logo": "<i class='fab fa-css3-alt'></i>",
+            }, {
+                "name": "JS",
+                "logo": "<i class='fab fa-js-square'></i>"
+            }
+        ],
+        "link": [{
+                "url": "https://github.com/ArthurTakase/Heros.js ",
+                "logo": "<i class='fab fa-github-alt'></i>"
+            },
+            {
+                "url": "https://arthurtakase.github.io/Heros.js/",
+                "logo": "<i class='fa-solid fa-play'></i>"
+            }
+        ],
+        "gif": "img/gif/herosjs.gif",
+        "img": "https://zupimages.net/up/21/39/rrpg.jpg"
+    },
+    {
+        "type": "perso",
+        "title": "Coding Club",
+        "description": "Littles projects to teach young people how to code.",
+        "tech": [{
+            "name": "Python",
+            "logo": "<i class='fab fa-python'></i>",
+        }, {
+            "name": "JS",
+            "logo": "<i class='fab fa-js-square'></i>"
+        }],
+        "link": [{
+                "url": "https://github.com/ArthurTakase/CodingClub",
+                "logo": "<i class='fab fa-github-alt'></i>"
+            },
+            {
+                "url": "https://arthurtakase.github.io/CodingClub/",
+                "logo": "<i class='fa-solid fa-play'></i>"
+            }
+        ],
+        "img": "https://zupimages.net/up/22/14/5i37.png"
+    },
+    {
+        "type": "perso",
+        "title": "OhMyCSS",
+        "description": "Lots of small projects mainly oriented around CSS.",
+        "tech": [{
+                "name": "HTML5",
+                "logo": "<i class='fab fa-html5'></i>",
+            },
+            {
+                "name": "CSS3",
+                "logo": "<i class='fab fa-css3-alt'></i>",
+            }
+        ],
+        "link": [{
+                "url": "https://github.com/ArthurTakase/OhMyCSS",
+                "logo": "<i class='fab fa-github-alt'></i>"
+            },
+            {
+                "url": "https://arthurtakase.github.io/OhMyCSS/",
+                "logo": "<i class='fa-solid fa-play'></i>"
+            }
+        ],
+        "gif": "img/gif/rubiks.gif",
+        "img": "img/projects/movie.png"
+    },
+    {
+        "type": "perso",
+        "title": "Epitech Bundle",
+        "description": "All projects at Epitech (tek1 & 2).",
+        "tech": [{
+                "name": "C",
+                "logo": "C",
+            },
+            {
+                "name": "Python",
+                "logo": "<i class='fab fa-python'></i>",
+            },
+            {
+                "name": "Shell",
+                "logo": "<i class='fa-solid fa-terminal'></i>",
+            },
+            {
+                "name": "JS",
+                "logo": "<i class='fab fa-js-square'></i>"
+            },
+            {
+                "name": "C++",
+                "logo": "<i class='bx bxl-c-plus-plus'></i>"
+            },
+            {
+                "name": "Haskell",
+                "logo": "λ"
+            }
+        ],
+        "link": [{
+            "url": "https://github.com/ArthurTakase/Epitech-Bundle",
+            "logo": "<i class='fab fa-github-alt'></i>"
+        }],
+        "img": "https://zupimages.net/up/22/14/f6ax.jpg"
+    },
+    {
+        "type": "perso",
+        "title": "LittleGamesBundle",
+        "description": "Compilation of the different games I've developed so far.",
+        "tech": [{
+                "name": "Construct 2",
+                "logo": "C2",
+            },
+            {
+                "name": "Python",
+                "logo": "<i class='fab fa-python'></i>",
+            },
+            {
+                "name": "JS",
+                "logo": "<i class='fab fa-js-square'></i>"
+            },
+            {
+                "name": "Unity",
+                "logo": "<i class='fa-brands fa-unity'></i>",
+            }
+        ],
+        "link": [{
+                "url": "https://github.com/ArthurTakase/LittleGamesBundle ",
+                "logo": "<i class='fab fa-github-alt'></i>"
+            },
+            {
+                "url": "https://arthurtakase.github.io/LittleGamesBundle",
+                "logo": "<i class='fa-solid fa-play'></i>"
+            }
+        ],
+        "img": "https://zupimages.net/up/21/39/hipn.png"
+    },
+    {
+        "type": "perso",
+        "title": "ArchivedPrograms",
+        "description": "Compilations of old programs.",
+        "tech": [{
+            "name": "Python",
+            "logo": "<i class='fab fa-python'></i>",
+        }],
+        "link": [{
+            "url": "https://github.com/ArthurTakase/ArchivedPrograms",
+            "logo": "<i class='fab fa-github-alt'></i>"
+        }],
+        "img": "https://zupimages.net/up/22/14/rtc1.jpg"
+    }
+]
